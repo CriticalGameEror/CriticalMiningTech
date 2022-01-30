843997c4df1e5070077b78763900cd2bd347cc19
CriticalGameEror <83310221+CriticalGameEror@users.noreply.github.com>
2022-01-30 17:39:26 +0000

    Added main and plugin.yml

---
+++ src/me/CriticalGameEror/mc/CriticalMiningTech.java
@@ -0,0 +1,15 @@
+package me.CriticalGameEror.mc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CriticalMiningTech extends JavaPlugin{
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+
+}
